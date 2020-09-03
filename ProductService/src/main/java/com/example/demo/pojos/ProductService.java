2acfb6b999500e79448dc5942e300f69b0ef1a6f
ANIKETB3 <ANIKETB3@D-10212.BIRLASOFT.COM>
2020-09-03 17:22:55 +0530

    separate category and size table

--- ProductService/src/main/java/com/example/demo/pojos/ProductService.java
+++ ProductService/src/main/java/com/example/demo/pojos/ProductService.java
@@ -18,14 +18,14 @@ public class ProductService {
 	@Column(name = "productName")
 	private String productName;
 	
-	@Column(name = "productCategory")
-	private String productCategory;
+	@Column(name = "categoryId")
+	private int categoryId;
 	
 	@Column(name = "productPrice")
 	private double productPrice;
 	
-	@Column(name = "productSize")
-	private String productSize;
+	@Column(name = "sizeId")
+	private int sizeId;
 	
 	@Column(name = "gender")
 	private String gender;
@@ -38,17 +38,43 @@ public class ProductService {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public ProductService(int productId, String productName, String productCategory, double productPrice,
-			String productSize, String gender, String productImages) {
+	public ProductService(int productId, String productName, int categoryId, double productPrice, int sizeId,
+			String gender, String productImages) {
 		super();
 		this.productId = productId;
 		this.productName = productName;
-		this.productCategory = productCategory;
+		this.categoryId = categoryId;
 		this.productPrice = productPrice;
-		this.productSize = productSize;
+		this.sizeId = sizeId;
 		this.gender = gender;
 		this.productImages = productImages;
 	}
+
+
+
+	public int getCategoryId() {
+		return categoryId;
+	}
+
+
+
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+
+
+
+	public int getSizeId() {
+		return sizeId;
+	}
+
+
+
+	public void setSizeId(int sizeId) {
+		this.sizeId = sizeId;
+	}
+
+
 
 	public int getProductId() {
 		return productId;
@@ -66,28 +92,12 @@ public class ProductService {
 		this.productName = productName;
 	}
 
-	public String getProductCategory() {
-		return productCategory;
-	}
-
-	public void setProductCategory(String productCategory) {
-		this.productCategory = productCategory;
-	}
-
 	public double getProductPrice() {
 		return productPrice;
 	}
 
 	public void setProductPrice(double productPrice) {
 		this.productPrice = productPrice;
-	}
-
-	public String getProductSize() {
-		return productSize;
-	}
-
-	public void setProductSize(String productSize) {
-		this.productSize = productSize;
 	}
 
 	public String getGender() {
