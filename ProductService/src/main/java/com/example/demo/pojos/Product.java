217371a4248b0ca9a179b2e94b8a97e15d5bb73d
Aniket Bidgar <ANIKETB3@birlasoft.com>
2020-09-09 12:48:59 +0530

    new files aded

---
+++ ProductService/src/main/java/com/example/demo/pojos/Product.java
@@ -0,0 +1,76 @@
+package com.example.demo.pojos;
+
+public class Product {
+	
+	private int productId;
+	
+	private String productName;
+	
+	private String categoryName;
+	
+	private double productPrice;
+	
+	private String sizeName;
+	
+	private String gender;
+	
+	private String productImages;
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public double getProductPrice() {
+		return productPrice;
+	}
+
+	public void setProductPrice(double productPrice) {
+		this.productPrice = productPrice;
+	}
+
+	public String getSizeName() {
+		return sizeName;
+	}
+
+	public void setSizeName(String sizeName) {
+		this.sizeName = sizeName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getProductImages() {
+		return productImages;
+	}
+
+	public void setProductImages(String productImages) {
+		this.productImages = productImages;
+	}
+
+	
+}
